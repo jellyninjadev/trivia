592def7efc9bd131d8a607d4d7fc937ae45e3491
Vlad Rimsha <vlad@jelly.ninja>
2021-01-08 19:09:16 +0700

    Submit missing files

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react'
+import {ActivityIndicator, Button, SafeAreaView, StyleSheet, Text, View} from 'react-native'
+import Api from './src/api'
+import {Question} from "./src/types"
+import Scene from "./src/Scene"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default () => {
+  const [started, setStarted] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [ready, setReady] = useState(false)
+  const [token, setToken] = useState('')
+  const [questions, setQuestions] = useState<Map<string, Question>>(new Map)
+
+  useEffect(() => {
+    const bootstrap = async () => {
+      setReady(true)
+      setLoading(false)
+    }
+
+    bootstrap()
+  }, [])
+
+  const start = async () => {
+    setLoading(true)
+    const newToken = await Api.token()
+    setToken(token)
+    const newQuestions = await Api.questions(newToken)
+    const questionMap = newQuestions.reduce((map, question, i) => map.set(i, question), new Map)
+    setQuestions(questionMap)
+    console.log('starting with questions', questionMap)
+    setLoading(false)
+    setStarted(true)
+  }
+
+  if (!ready) return <View><ActivityIndicator /></View>
+
+  if (!started) return <SafeAreaView>
+    <Text>Trivia</Text>
+    <Text>A trivia game or competition is one where the competitors are asked questions about interesting but unimportant facts in mane subjects</Text>
+    <Button title="Start quiz" onPress={start} />
+  </SafeAreaView>
+
+  if (loading) return <View><ActivityIndicator /></View>
+
+  return <Scene questions={questions} restart={start} />
 }
 
 const styles = StyleSheet.create({
