b4f7d0d0a9a14318f1bb0afdaa642af0d879bef5
Vlad Rimsha <vlad@jelly.ninja>
2022-07-11 20:44:31 +0700

    fix: improved circleci

--- e2e/main.e2e.js
+++ e2e/main.e2e.js
@@ -6,7 +6,7 @@ function sleep(ms) {
 
 describe('Example', () => {
   beforeEach(async () => {
-		await reloadApp();
+		await reloadApp({url: 'exp://exp.host/@senhorbardell/trivia?release-channel=detox'});
 		await waitFor(element(by.id('StartButton'))).toBeVisible().withTimeout(5000);
   });
 
